5ba91cbaf2244ff4409bfed1ef458a7e8a52b3f1
Jonah Leibowitz <jleibowitz@ucvts.org>
2018-11-06 13:10:20 -0500

    trying to push

--- functions.js
+++ functions.js
@@ -1,1 +1,18 @@
-console.log("Hello, Console")
+
+function helloconsole() {
+  console.log("Hello, World");
+}
+
+function hellopopup() {
+  alert ("Hello, World");
+}
+
+function hellobrowser() {
+  var p=document.getElementById("HelloWorld");
+  p.innerHTML="Hello, World";
+}
+
+function hellostranger() {
+  var name= prompt("Enter your name : ", "your name here");
+  alert ("Hello " + name);
+}
